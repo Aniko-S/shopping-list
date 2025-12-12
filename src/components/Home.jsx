75928ad06602a135aabd586fd2b33cd6c4b9802a
aniko <s.aniko9424@gmail.com>
2025-12-12 15:28:39 +0100

    Get product list
    Get product list and show products name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,40 @@
+import { db } from "../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 function Home() {
-  return <div>Bejelentkezve</div>;
+  const [productList, setProductList] = useState([]);
+
+  const docRef = collection(db, "product_list");
+
+  useEffect(() => {
+    loadProductList();
+  }, []);
+
+  const loadProductList = async () => {
+    try {
+      const docSnap = await getDocs(docRef);
+      const data = docSnap.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      setProductList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Bevásárlólista</h1>
+      <div>
+        {productList.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Home;
